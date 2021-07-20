11f9af179be342c0e04b6be22169994417784588
Pablo Bonilla <pbonilla29@hotmail.com>
2021-07-20 11:26:30 -0600

    Add provinces list with images

--- src/index.js
+++ src/index.js
@@ -1,18 +1,2 @@
 import './styles/main.scss';
-import ambienceSound from './audio/ambience.mp3';
-
-document.addEventListener('DOMContentLoaded', e => {
-  document.getElementById('js-launch-website').addEventListener('click', e => {
-    launchWebsitePreloader();
-  });
-});
-
-const launchWebsitePreloader = () => {
-  document.querySelector('#js-preloader').style.top = '-100vh';
-  let audio = new Audio(ambienceSound);
-  setTimeout(e => {
-    audio.play();
-    audio.volume = 0.35;
-    audio.loop = true;
-  }, 850);
-};
+import './js/preloader';
---
+++ src/js/preloader.js
@@ -0,0 +1,28 @@
+import ambienceSound from './../audio/ambience.mp3';
+
+document.addEventListener('DOMContentLoaded', e => {
+  const websiteContent = document.getElementById('js-website-content');
+  const jsPreloader = document.getElementById('js-preloader');
+  
+  // websiteContent.style.display = 'none';
+  jsPreloader.style.display = 'none';
+
+  document.getElementById('js-launch-website').addEventListener('click', e => {
+    launchWebsitePreloader();
+  });
+});
+
+const launchWebsitePreloader = () => {
+  const jsPreloader = document.getElementById('js-preloader');
+  const websiteContent = document.getElementById('js-website-content');
+
+  jsPreloader.style.top = '-100vh';
+  let audio = new Audio(ambienceSound);
+  setTimeout(e => {
+    audio.play();
+    audio.volume = 0.35;
+    audio.loop = true;
+    jsPreloader.style.display = 'none';
+    websiteContent.style.display = 'block';
+  }, 850);
+};
